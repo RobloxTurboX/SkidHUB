a7dfc3a8814f42a9ad9c8b5d3ea82bc2894728dd
TheBayHarbourSkidder <127440417+RobloxTurboX@users.noreply.github.com>
2025-11-01 02:06:06 +0300

    Update SkidHubLoader.lua

--- SkidHubLoader.lua
+++ SkidHubLoader.lua
@@ -1,7 +1,7 @@
 -- Hey hey hey little skid 💀🥀 never skid my scripts cuz i skidded them already🥭🥭🥭
 -- Skid, eat, sleep, repeat
 
-print("ToNight's The Night")
+print("Tonight's the night. And it's going to happen again... and again. It has to happen. Nice night. Miami is a great town. I love the Cuban food, pork sandwiches, my favorite. But I'm hungry for something different now")
 print("Skid HUB loader...")
 print("Loading script...")
 
@@ -10,4 +10,5 @@ if game.PlaceId == 125009265613167 then
 else
     error("Unsupported Game! Please execute loader in supported Game")
 end
+
 
