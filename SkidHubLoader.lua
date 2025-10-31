368afb95d06e51d058d51d44ea9c2fc4c3dc5e52
TheBayHarbourSkidder <127440417+RobloxTurboX@users.noreply.github.com>
2025-11-01 02:02:36 +0300

    Add files via upload

---
+++ SkidHubLoader.lua
@@ -0,0 +1,13 @@
+-- Hey hey hey little skid 💀🥀 never skid my scripts cuz i skidded them already🥭🥭🥭
+-- Skid, eat, sleep, repeat
+
+print("ToNight's The Night")
+print("Skid HUB loader...")
+print("Loading script...")
+
+if game.PlaceId == 125009265613167 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/InkGame.lua"))()
+else
+    error("Unsupported Game! Please execute loader in supported Game")
+end
+
