19e2a0c1db2ce9481c279dbd577de264b1907b56
TheBayHarbourSkidder <127440417+RobloxTurboX@users.noreply.github.com>
2025-11-04 16:24:10 +0300

    Update SkidHubLoader.lua

--- SkidHubLoader.lua
+++ SkidHubLoader.lua
@@ -6,10 +6,11 @@ print("Skid HUB loader...")
 print("Loading script...")
 
 if game.PlaceId == 125009265613167 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/InkGame"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/InkGame.lua"))()
 elseif game.PlaceId == 119099244949868 or game.PlaceId == 99567941238278 or game.PlaceId == 76172769094087 or game.PlaceId == 113555439745862 or game.PlaceId == 121431824618615 or game.PlaceId == 122816944483266 or game.PlaceId == 135136333168784 or game.PlaceId == 138384426832196 then
     return error("LOAD IN GAME NOT IN LOBBY🙏😭🥀")
 else
     return error("Unsupported Game! Please execute loader in supported Game")
 end
+
 
