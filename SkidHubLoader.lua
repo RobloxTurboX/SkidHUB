6b7253515d25e646f918523398b005746442dfc8
TheBayHarbourSkidder <127440417+RobloxTurboX@users.noreply.github.com>
2025-12-18 17:00:04 +0300

    Update SkidHubLoader.lua

--- SkidHubLoader.lua
+++ SkidHubLoader.lua
@@ -7,11 +7,14 @@ print("Loading script...")
 
 if game.PlaceId == 125009265613167 or game.PlaceId == 122816944483266 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/InkGame.lua"))()
-elseif game.PlaceId == 119099244949868 or game.PlaceId == 99567941238278 or game.PlaceId == 76172769094087 or game.PlaceId == 113555439745862 or game.PlaceId == 121431824618615 or game.PlaceId == 135136333168784 or game.PlaceId == 138384426832196 then
+elseif game.PlaceId == 93044798454681 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/DeadlyDelivery.lua"))()
+elseif game.PlaceId == 119099244949868 or game.PlaceId == 99567941238278 or game.PlaceId == 76172769094087 or game.PlaceId == 113555439745862 or game.PlaceId == 121431824618615 or game.PlaceId == 135136333168784 or game.PlaceId == 138384426832196 or 125810438250765 then
     return error("LOAD IN GAME NOT IN LOBBY🙏😭🥀")
 else
     return error("Unsupported Game! Please execute loader in supported Game")
 end
+
 
 
 
