0a8bb3de167846f0e65ed11476bb8e46321f6b15
TheBayHarbourSkidder <127440417+RobloxTurboX@users.noreply.github.com>
2026-03-17 22:25:24 +0300

    Update SkidHubLoader.lua

--- SkidHubLoader.lua
+++ SkidHubLoader.lua
@@ -7,6 +7,8 @@ if game.PlaceId == 125009265613167 or game.PlaceId == 122816944483266 or game.Pl
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/InkGame.lua"))()
 elseif game.PlaceId == 93044798454681 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/DeadlyDelivery.lua"))()
+elseif game.PlaceId == 2809202155 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/YBA.luau"))()
 elseif game.PlaceId == 119099244949868 or game.PlaceId == 99567941238278 or game.PlaceId == 76172769094087 or game.PlaceId == 113555439745862 or game.PlaceId == 121431824618615 or game.PlaceId == 135136333168784 or game.PlaceId == 138384426832196 or 125810438250765 then
     return error("LOAD IN GAME NOT IN LOBBY🙏😭🥀")
 else
