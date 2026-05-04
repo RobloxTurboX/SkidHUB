15443df114d25f376f802fb0c169a5294a7e03c8
TheBayHarbourSkidder <127440417+RobloxTurboX@users.noreply.github.com>
2026-05-04 19:10:51 +0300

    Update SkidHubLoader.lua

--- SkidHubLoader.lua
+++ SkidHubLoader.lua
@@ -5,14 +5,12 @@ print("Skid Hub loader, loading script...")
 
 if game.PlaceId == 125009265613167 or game.PlaceId == 122816944483266 or game.PlaceId == 90407707718652 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/InkGame.lua"))()
-elseif game.PlaceId == 93044798454681 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/DeadlyDelivery.lua"))()
 elseif game.PlaceId == 2809202155 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/YBA.luau"))()
 elseif game.PlaceId == 537413528 then
     loadstring(game:HttpGet("https://github.com/RobloxTurboX/SkidHUB/raw/refs/heads/main/Babft.luau"))()
-elseif game.PlaceId == 91731139776520 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/Cubination.lua"))()
+elseif game.GameId == 8524572339 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/B%3AW.luau"))()
 elseif game.PlaceId == 119099244949868 or game.PlaceId == 99567941238278 or game.PlaceId == 76172769094087 or game.PlaceId == 113555439745862 or game.PlaceId == 121431824618615 or game.PlaceId == 135136333168784 or game.PlaceId == 138384426832196 or 125810438250765 then
     return error("LOAD IN GAME NOT IN LOBBY🙏😭🥀")
 else
