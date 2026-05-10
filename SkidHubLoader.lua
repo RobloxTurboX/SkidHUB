5aaec7036aa66de40ee3a760ba26f22e429b2966
TheBayHarbourSkidder <127440417+RobloxTurboX@users.noreply.github.com>
2026-05-10 13:47:50 +0300

    Update SkidHubLoader.lua

--- SkidHubLoader.lua
+++ SkidHubLoader.lua
@@ -1,20 +1,23 @@
--- Hey hey hey little skid 💀🥀 never skid my scripts cuz i skidded them already🥭🥭🥭
 -- Skid, eat, sleep, repeat
 
-print("Skid Hub loader, loading script...")
+print("Debug: Skid Hub loader - checking for Supported Game...")
 
 if game.PlaceId == 125009265613167 or game.PlaceId == 122816944483266 or game.PlaceId == 90407707718652 or game.PlaceId == 99567941238278 then
+    print("Debug: Skid Hub loader - Loading Ink Game...")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/InkGame.lua"))()
 elseif game.PlaceId == 2809202155 then
+    print("Debug: Skid Hub loader - Loading YBA...")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/YBA.luau"))()
 elseif game.PlaceId == 537413528 then
+    print("Debug: Skid Hub loader - Loading Babft...")
     loadstring(game:HttpGet("https://github.com/RobloxTurboX/SkidHUB/raw/refs/heads/main/Babft.luau"))()
 elseif game.GameId == 8524572339 then
+    print("Debug: Skid Hub loader - Loading B:W...")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxTurboX/SkidHUB/refs/heads/main/B%3AW.luau"))()
 elseif game.PlaceId == 119099244949868 or game.PlaceId == 76172769094087 or game.PlaceId == 113555439745862 or game.PlaceId == 121431824618615 or game.PlaceId == 135136333168784 or game.PlaceId == 138384426832196 or 125810438250765 then
-    return error("LOAD IN GAME NOT IN LOBBY🙏😭🥀")
+    return error("Debug: LOAD IN GAME NOT IN LOBBY T_T")
 else
-    return error("Unsupported Game! Please execute loader in supported Game")
+    return error("Debug: Unsupported Game! Please execute loader in supported Game")
 end
 
 
